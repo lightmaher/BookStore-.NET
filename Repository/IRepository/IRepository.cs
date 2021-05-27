23ed2e590dfb7ff93596255f2470801348bb9122
lightmaher <lightmaher3@gmail.com>
2021-05-27 11:56:47 +0200

    repoistory

---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.IRepository
+{
+    interface IRepository<T> where T : class
+    {
+        T GetT(int id);
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>,IOrderedQueryable<T>> OrderBy = null,
+            string includeproprites = null
+            );
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeproprites = null
+            );
+
+        void add(T entity);
+        void remove(int id);
+        void remove(T entity);
+        void removeRange(IEnumerable<T> entitis);
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,94 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy = null, string includeproprites = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeproprites != null)
+            {
+                foreach (var includeProp in includeproprites.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (OrderBy != null)
+            {
+                return OrderBy(query).ToList();
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeproprites = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeproprites != null)
+            {
+                foreach (var includeProp in includeproprites.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+
+            return query.FirstOrDefault();
+        }
+
+        public T GetT(int id)
+        {
+            return dbSet.Find(id);
+
+        }
+
+        public void remove(int id)
+        {
+            T entity = dbSet.Find(id);
+            remove(entity);
+        }
+
+        public void remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void removeRange(IEnumerable<T> entitis)
+        {
+            dbSet.RemoveRange(entitis);
+        }
+    }
+}
